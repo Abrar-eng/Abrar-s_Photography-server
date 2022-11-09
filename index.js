6266e17aa2eda401154ea59c634ecef40bd746c1
Abrar-eng <abrarulhasan600@gmail.com>
2022-11-10 01:32:00 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require ('express');
 const cors = require ('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.panqagk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.use(cors());
@@ -11,6 +22,6 @@ app.get('/', (req, res) =>{
     res.send('service running')
 })
 
-app.listen(post, () =>{
-    console.log(`server running ${port}`)
+app.listen(port, () =>{
+    console.log(`server running ${port}`);
 })
